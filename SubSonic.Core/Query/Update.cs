0d1a518abaf725d8c83e4b1a74482cd0fb6a1e0b
saintedlama <Christoph.Walcher@gmail.com>
2010-03-11 22:48:43 +0100

    Using qualified name for updates again

--- SubSonic.Core/Query/Update.cs
+++ SubSonic.Core/Query/Update.cs
@@ -190,7 +190,7 @@ namespace SubSonic.Query
             Setting s = new Setting
                             {
                                 query = this,
-                                ColumnName = column.Name,
+                                ColumnName = column.QualifiedName,
                                 ParameterName = (_provider.ParameterPrefix + "up_" + column.Name),
                                 IsExpression = isExpression,
                                 DataType = column.DataType
