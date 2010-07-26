ba64e1f161ebe7c8899368a52d20f96b8d43a1e2
stimms <stimms@gmail.com>
2010-07-26 10:51:00 -0600

    Adding some missing files

---
+++ SubSonic.Core/DataProviders/ServiceLocators/MefServiceLocator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+
+using Microsoft.Practices.ServiceLocation;
+
+
+namespace Microsoft.Mef.CommonServiceLocator
+{
+    public class MefServiceLocator : ServiceLocatorImplBase
+    {
+        private ExportProvider provider;
+
+        public MefServiceLocator(ExportProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            if (key == null)
+            {
+                key = AttributedModelServices.GetContractName(serviceType);
+            }
+
+            IEnumerable<Lazy<object>> exports = provider.GetExports<object>(key);
+
+            if (exports.Any())
+            {
+                return exports.First().Value;
+            }
+
+            throw new ActivationException(string.Format("Cannot locate instances of contract {0}", key));
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            var exports = provider.GetExportedValues<object>(AttributedModelServices.GetContractName(serviceType));
+            return exports;
+        }
+    }
+}
